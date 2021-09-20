3276c6636a542679387cfdd7256fc4e8a5dde5cd
Kenyon Luce <kenyon.luce02@gmail.com>
2021-09-20 16:08:14 -0500

    added controller

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Web.Mvc;
+using Vidly.Models
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Shrek!"};
+            
+            return View();
+        }
+    }
+}
