bd1243af2be534e4e291ff8540050dfb258bb50c
Kenyon Luce <kenyon.luce02@gmail.com>
2021-09-20 15:44:30 -0500

    first push

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c__exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
