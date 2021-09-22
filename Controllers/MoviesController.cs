e77a46b8fa2cb252296c38537b51a8aa59b3dccb
Kenyon Luce <kenyon.luce02@gmail.com>
2021-09-22 10:17:22 -0500

    saving progress

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,10 +1,10 @@
 using System;
-using System.Web.Mvc;
+// using System.Web.Mvc;
 using c__exercises.Models;
 
 namespace c__exercises.Controllers
 {
-    public class MoviesController : Controller
+    public class MoviesController : //Controller
     {
         //GET: Movies/Random
         public ActionResult Random()
